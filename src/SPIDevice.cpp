90947d6b15befbebe97b1113418e70a6b32d452c
Tijs van Roon <1374848+knifter@users.noreply.github.com>
2023-03-07 14:08:26 +0100

    added various read/write functions for lib-MAX31856

--- src/SPIDevice.cpp
+++ src/SPIDevice.cpp
@@ -5,18 +5,121 @@
 #include <Arduino.h>
 #include <SPI.h>
 
+bool SPIDevice::begin()
+{
+    pinMode(_cs, OUTPUT);
+    digitalWrite(_cs, HIGH);
+    
+    _spi.begin();
+
+    _initialized = true;
+    return true;
+};
+
 uint8_t SPIDevice::write(const uint8_t data[], const uint8_t len)
 {
-    _spi.beginTransaction(_spisettings);
     digitalWrite(_cs, LOW);
-    uint8_t cnt = 0;
-    while(cnt++ != len)
-        _spi.transfer(data[cnt]);
+    _spi.beginTransaction(_spisettings);
+    // uint8_t cnt = 0;
+    // while(cnt++ != len)
+    //     _spi.transfer(data[cnt]);
+    _spi.transferBytes(data, NULL, len);
     digitalWrite(_cs, HIGH);
     _spi.endTransaction();
 
+    return len;
+};
+
+uint8_t SPIDevice::read(uint8_t* data, const uint8_t len)
+{
+    digitalWrite(_cs, LOW);
+    _spi.beginTransaction(_spisettings);
+    uint8_t cnt = 0;
+    while(cnt != len)
+        data[cnt++] = _spi.transfer(0);
+    _spi.endTransaction();
+    digitalWrite(_cs, HIGH);
+
     return cnt;
-}
+};
+
+void SPIDevice::writeread(const uint8_t *wbuf, const size_t wlen, 
+                                uint8_t *rbuf, const size_t rlen, 
+                          const uint8_t rvalue) 
+{
+    digitalWrite(_cs, LOW);
+    _spi.beginTransaction(_spisettings);
+
+    _spi.transferBytes(wbuf, NULL, wlen);
+
+    uint8_t rcnt = 0;
+    while(rcnt != rlen)
+        rbuf[rcnt++] = _spi.transfer(rvalue);
+
+    _spi.endTransaction();
+    digitalWrite(_cs, HIGH);
+};
+
+uint8_t SPIDevice::readreg8(const uint8_t reg)
+{
+    digitalWrite(_cs, LOW);
+    _spi.beginTransaction(_spisettings);
+
+    // Write address (MSB=0 -> read)
+    _spi.transfer(reg & 0x7F);
+
+    uint8_t r = _spi.transfer(0x00);
+
+    _spi.endTransaction();
+    digitalWrite(_cs, HIGH);
+
+    return r;
+};
+
+void SPIDevice::writereg8(const uint8_t reg, const uint8_t data)
+{
+    write16(((reg | 0x80) << 8) | data);
+};
+
+uint16_t SPIDevice::readreg16(const uint8_t reg)
+{
+    digitalWrite(_cs, LOW);
+    _spi.beginTransaction(_spisettings);
+
+    // Write address (MSB=0 -> read)
+    _spi.transfer(reg & 0x7F);
+
+    uint16_t r = _spi.transfer16(0x0000);
+
+    _spi.endTransaction();
+    digitalWrite(_cs, HIGH);
+
+    return r;
+};
+
+uint32_t SPIDevice::readreg24(const uint8_t reg)
+{
+    digitalWrite(_cs, LOW);
+    _spi.beginTransaction(_spisettings);
+
+    // Write address (MSB=0 -> read)
+    _spi.transfer(reg & 0x7F);
+
+    union
+    {
+        uint8_t raw[4];
+        uint32_t i;
+    } r;
+    r.raw[0] = _spi.transfer(0x00);
+    r.raw[1] = _spi.transfer(0x00);
+    r.raw[2] = _spi.transfer(0x00);
+    r.raw[3] = _spi.transfer(0x00);
+
+    _spi.endTransaction();
+    digitalWrite(_cs, HIGH);
+
+    return r.i;
+};
 
 void SPIDevice::write16(const uint16_t v)
 {
@@ -25,24 +128,16 @@ void SPIDevice::write16(const uint16_t v)
     _spi.transfer16(v);
     digitalWrite(_cs, HIGH);
     _spi.endTransaction();
-}
+};
 
 uint32_t SPIDevice::read32(void) 
 { 
-    uint32_t d = 0;
+    union {
+        uint8_t raw[4];
+        uint32_t i;
+    } data;
 
-    // Do beginTransaction after setting CS low to introduce a little delay after CS
-    digitalWrite(_cs, LOW);
-    _spi.beginTransaction(_spisettings);
-    d = _spi.transfer(0);
-    d <<= 8;
-    d |= _spi.transfer(0);
-    d <<= 8;
-    d |= _spi.transfer(0);
-    d <<= 8;
-    d |= _spi.transfer(0);
-    digitalWrite(_cs, HIGH);
-    _spi.endTransaction();
+    read(data.raw, sizeof(uint32_t));
+    return data.i;
+};
 
-    return d;
-}
--- src/SPIDevice.h
+++ src/SPIDevice.h
@@ -18,13 +18,27 @@ class SPIDevice
             : _spi(spi), _cs(cs), _spisettings(clock, bitOrder, dataMode) { }
 
     protected:
-        SPIClass& _spi;
-        int8_t _cs;
+        SPIClass&   _spi;
+        int8_t      _cs;
         SPISettings _spisettings;
+        bool        _initialized = false;
 
-        uint8_t write(const uint8_t data[], const uint8_t len);
-        void write16(const uint16_t);
-        uint32_t read32();
+        bool        begin();
+
+        uint8_t     write(const uint8_t data[], const uint8_t len);
+        uint8_t     read(uint8_t* data, const uint8_t len);
+
+        void        writeread(const uint8_t *wbuf, const size_t wlen, 
+                                    uint8_t *rbuf, const size_t rlen, 
+                              const uint8_t rvalue = 0x00);
+
+        uint8_t     readreg8(const uint8_t reg);
+        void        writereg8(const uint8_t reg, const uint8_t data);
+        uint16_t    readreg16(const uint8_t reg);
+        uint32_t    readreg24(const uint8_t reg);
+
+        void        write16(const uint16_t);
+        uint32_t    read32();
 };
 
 #endif //__SPIDEVICE_H
